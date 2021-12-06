04de437f42a9a52a52d1a8f824f1d8a46accd597
Darren Tebo <darrentebo83@outlook.com>
2021-12-06 10:12:25 -0500

    Added Humans Vs Goblins Project

---
+++ HumansVsGoblins/src/com/company/Land.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Land {
+    /*NOTE: 0 = Empty Space,
+            1 = Human,
+            2 = Goblin,
+            3 = Drop,
+            4 = Treasure
+     */
+    private int[][] board = {{0},{0},{0},{0},{0},
+                             {0},{0},{0},{0},{0},
+                             {0},{0},{0},{0},{0},
+                             {0},{0},{0},{0},{0},
+                             {0},{0},{0},{0},{0},};
+
+
+}
