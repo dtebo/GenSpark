24cf627f7b6376d79354051b80e0ea02e99ff604
Darren Tebo <darrentebo83@outlook.com>
2021-12-20 10:08:02 -0500

    Added hangman parts txt file

--- HangmanFunctional/src/com/company/Game.java
+++ HangmanFunctional/src/com/company/Game.java
@@ -1,4 +1,52 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Game {
+    private List<String> words;
+
+    public Game() {
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public void setWords(List<String> words) {
+        this.words = words;
+    }
+
+    public String getGameWord(){
+        int random = ThreadLocalRandom.current().nextInt(0, getWords().size() - 1);
+
+        return getWords().get(random);
+    }
+
+    public void printBoard(String filePath) throws IOException {
+        String boardState = Files.readString(Paths.get(filePath));
+    }
+
+    public List<String> readWords(String filePath) throws IOException {
+        List<String> fileWords = new ArrayList<>();
+
+        fileWords = Files.readAllLines(Paths.get(filePath));
+
+        return fileWords;
+    }
+
+    public void start() throws IOException {
+        try{
+            setWords(readWords("C:\\GenSpark\\GenSpark\\HangmanFunctional\\src\\com\\company\\words.txt"));
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+
+
+    }
 }
--- HangmanFunctional/src/com/company/Main.java
+++ HangmanFunctional/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        Game game = new Game();
+        game.start();
     }
 }
